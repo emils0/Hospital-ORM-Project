2918a1e8b9ec8a55390f8d3f4dc1e75f8c9f8c4a
emil <89942504+emils0@users.noreply.github.com>
2023-08-14 14:35:22 +0200

    Made some changes.

--- Program.cs
+++ Program.cs
@@ -1,21 +1,51 @@
 using Hospital_ORM_project;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
 
-Console.WriteLine("test");
+namespace Hospital_ORM_project {
+	public static class Program {
+		public static void Main(string[] args) {
+			Console.WriteLine("Starting the Hospital Management System...");
 
-public class HospitalContext : DbContext
-{
-	public DbSet<Doctor> Doctors { get; set; }
-	public DbSet<Nurse> Nurses { get; set; }
-	public DbSet<Patient> Patients { get; set; }
-	public DbSet<Appointment> Appointments { get; set; }
-	public DbSet<MedicalRecord> MedicalRecords { get; set; }
+			// Create instances
+			var doctor = new Doctor
+				{Name = "Dr. Smith", Qualification = "MD", Experience = 10, Specialty = "Cardiology"};
+			var nurse = new Nurse
+				{Name = "Nurse Johnson", Qualification = "RN", Experience = 5, Department = "Surgery" };
+			var patient = new Patient {Name = "John Doe"};
+			var medicalRecord = new MedicalRecord {Details = "Medical history details", Patient = patient};
+			var appointment = new Appointment {Date = DateTime.Now, Doctor = doctor, Patient = patient};
 
-	protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-	{
-		optionsBuilder.UseMySql("Server=localhost;Database=HospitalDB;User=root;", 
-			new MySqlServerVersion(new Version(11, 0, 2)));
+			// Add instances to the database
+			using (var context = new HospitalContext()) {
+				context.Database.EnsureCreated(); // Ensure the database is created
+
+				context.Doctors.Add(doctor);
+				context.Nurses.Add(nurse);
+				context.Patients.Add(patient);
+				context.MedicalRecords.Add(medicalRecord);
+				context.Appointments.Add(appointment);
+
+				context.SaveChanges(); // Save changes to the database
+			}
+
+			Console.WriteLine("Data added successfully!");
+		}
 	}
 
-}
+	public class HospitalContext : DbContext
+		// ... rest of the code ...
+	{
+		public DbSet<Doctor> Doctors { get; set; }
+		public DbSet<Nurse> Nurses { get; set; }
+		public DbSet<Patient> Patients { get; set; }
+		public DbSet<Appointment> Appointments { get; set; }
+		public DbSet<MedicalRecord> MedicalRecords { get; set; }
 
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+			optionsBuilder.UseMySql("Server=localhost;Database=HospitalDB;User=root;",
+				new MySqlServerVersion(new Version(11, 0, 2)));
+		}
+	}
+}
